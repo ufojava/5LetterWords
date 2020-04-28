f9cd1e2ef906387b5e72a6d521608e8709e438b9
Ufuoma Okoro <ufookoro@gmail.com>
2020-04-28 21:59:10 +0100

    Completed User Interface

--- 5LetterWords/Views/GameMain.swift
+++ 5LetterWords/Views/GameMain.swift
@@ -78,6 +78,30 @@ struct Game: View {
     @State private var playerSortedThirdLetter = ""
     @State private var playerSortedFourthLetter = ""
     @State private var playerSortedFifthLetter = ""
+    
+    
+    //Played Letters
+    @State private var showPlayedLetters = false
+    
+    //Played Letters One
+    @State private var playedLetterOne = ""
+    @State private var playedLetterOneState = false
+    
+    //Played Letters Two
+    @State private var playedLetterTwo = ""
+    @State private var playedLetterTwoState = false
+    
+    //Played Letters Three
+    @State private var playedLetterThree = ""
+    @State private var playedLetterThreeState = false
+    
+    //Played Letters Four
+    @State private var playedLetterFour = ""
+    @State private var playedLetterFourState = false
+    
+    //Played Letters Five
+    @State private var playedLetterFive = ""
+    @State private var playedLetterFiveState = false
 
     
     
@@ -153,7 +177,7 @@ struct Game: View {
     var body: some View {
         
         
-        ZStack {
+        ZStack(alignment: .top) {
             
             //Set background
             LinearGradient(gradient: Gradient(colors: [.blue, .white,.green]), startPoint: .topLeading, endPoint: .bottomTrailing)
@@ -161,8 +185,10 @@ struct Game: View {
         
                 VStack {
                 
-                        Text("5 Letter Dictionary")
-            
+                        Text("5 Letter Scramble")
+                            .font(.largeTitle)
+                            
+                        Spacer().frame(height:40)
                         
                     Button(action: {
                         
@@ -190,56 +216,320 @@ struct Game: View {
                     }) {
                         
                         
-                        Text("Get Random Word")
+                        Text("Get Word")
+                            .frame(width:100,height: 30)
+                            .background(Color.gray)
+                            .foregroundColor(Color.black)
+                            .overlay(RoundedRectangle(cornerRadius: 5).stroke(Color.black,lineWidth: 1))
+                            
                     }
+                    
+                    Spacer().frame(height:30)
                     
                     if self.newRandomWord {
                         
-                       
+                      
                         
-                        Text("To be hidden: \(self.displayRandomWord)")
-                        Text("For Player \(self.sortedRandomWord)")
-                        Text("For Tiles: \(self.shuffledRandomWord)")
                         
-                        Text("Random Letters for Tiles")
+                        //Text("To be hidden: \(self.displayRandomWord)")
+                        //Text("For Tiles: \(self.shuffledRandomWord)")
                         
+                        //Shuffled Random Letter
+                        
+                        Spacer().frame(height:40)
+                        Text("Random Word")
+                            .font(.custom("Chalkboard SE", size: 25))
                         Spacer().frame(height:10)
-                        
-                        HStack {
-                        Text("\(self.randomFirstLetter)")
-                        Text("\(self.randomSecondLetter)")
-                        Text("\(self.randomThirdLetter)")
-                        Text("\(self.randomFourthLetter)")
-                        Text("\(self.randomFifthLetter)")
-                        }
-                       
-                        
-                        Spacer().frame(height:10)
-                        Text("Player Letter Pad")
-                        Spacer().frame(height:10)
-                        
                         HStack {
                             
-                            Text("\(self.playerSortedFirstLetter)")
-                            Text("\(self.playerSortedSecondLetter)")
-                            Text("\(self.playerSortedThirdLetter)")
-                            Text("\(self.playerSortedFourthLetter)")
-                            Text("\(self.playerSortedFifthLetter)")
-                            
-                            
-                            
-                            
-                        }
+                            RandomLetterSquare(inletter: self.randomFirstLetter)
+                            RandomLetterSquare(inletter: self.randomSecondLetter)
+                            RandomLetterSquare(inletter: self.randomThirdLetter)
+                            RandomLetterSquare(inletter: self.randomFourthLetter)
+                            RandomLetterSquare(inletter: self.randomFifthLetter)
                         
                         
+                        }//End of Random HStack
                         
-                    }
+                        
+                       
+                        
+                        Spacer().frame(height:40)
+                        Text("Player Letters Pad")
+                            .font(.custom("Chalkboard SE", size: 25))
+                        Spacer().frame(height:10)
+                        
+                       //Sorted Random Letter
+                        HStack {
+                            
+                            SortedLetterSquare(inletter: self.playerSortedFirstLetter)
+                                .onTapGesture {
+                                    if self.playedLetterOne == "" {
+                                        
+                                        self.playedLetterOne = self.playerSortedFirstLetter
+                                        
+                                    } else if self.playedLetterTwo == "" {
+                                        
+                                        self.playedLetterTwo = self.playerSortedFirstLetter
+                                        
+                                    } else if self.playedLetterThree == "" {
+                                        
+                                        self.playedLetterThree = self.playerSortedFirstLetter
+                                        
+                                    } else if self.playedLetterFour == "" {
+                                        
+                                        self.playedLetterFour = self.playerSortedFirstLetter
+                                        
+                                    } else if self.playedLetterFive == "" {
+                                        
+                                        self.playedLetterFive = self.playerSortedFirstLetter
+                                    }
+                            }//End of TapGesture
+                            
+                            
+                            SortedLetterSquare(inletter: self.playerSortedSecondLetter)
+                                .onTapGesture {
+                                        if self.playedLetterOne == "" {
+                                            
+                                            self.playedLetterOne = self.playerSortedSecondLetter
+                                            
+                                        } else if self.playedLetterTwo == "" {
+                                            
+                                            self.playedLetterTwo = self.playerSortedSecondLetter
+                                            
+                                        } else if self.playedLetterThree == "" {
+                                            
+                                            self.playedLetterThree = self.playerSortedSecondLetter
+                                            
+                                        } else if self.playedLetterFour == "" {
+                                            
+                                            self.playedLetterFour = self.playerSortedSecondLetter
+                                            
+                                        } else if self.playedLetterFive == "" {
+                                            
+                                            self.playedLetterFive = self.playerSortedSecondLetter
+                                        }
+                                }//End of TapGesture
+                            
+                            
+                            SortedLetterSquare(inletter: self.playerSortedThirdLetter)
+                                .onTapGesture {
+                                        if self.playedLetterOne == "" {
+                                            
+                                            self.playedLetterOne = self.playerSortedThirdLetter
+                                            
+                                        } else if self.playedLetterTwo == "" {
+                                            
+                                            self.playedLetterTwo = self.playerSortedThirdLetter
+                                            
+                                        } else if self.playedLetterThree == "" {
+                                            
+                                            self.playedLetterThree = self.playerSortedThirdLetter
+                                            
+                                        } else if self.playedLetterFour == "" {
+                                            
+                                            self.playedLetterFour = self.playerSortedThirdLetter
+                                            
+                                        } else if self.playedLetterFive == "" {
+                                            
+                                            self.playedLetterFive = self.playerSortedThirdLetter
+                                        }
+                                }//End of TapGesture
+                                
+                            SortedLetterSquare(inletter: self.playerSortedFourthLetter)
+                                .onTapGesture {
+                                        if self.playedLetterOne == "" {
+                                            
+                                            self.playedLetterOne = self.playerSortedFourthLetter
+                                            
+                                        } else if self.playedLetterTwo == "" {
+                                            
+                                            self.playedLetterTwo = self.playerSortedFourthLetter
+                                            
+                                        } else if self.playedLetterThree == "" {
+                                            
+                                            self.playedLetterThree = self.playerSortedFourthLetter
+                                            
+                                        } else if self.playedLetterFour == "" {
+                                            
+                                            self.playedLetterFour = self.playerSortedFourthLetter
+                                            
+                                        } else if self.playedLetterFive == "" {
+                                            
+                                            self.playedLetterFive = self.playerSortedFourthLetter
+                                        }
+                                }//End of TapGesture
+                            SortedLetterSquare(inletter: self.playerSortedFifthLetter)
+                                .onTapGesture {
+                                        if self.playedLetterOne == "" {
+                                            
+                                            self.playedLetterOne = self.playerSortedFifthLetter
+                                            
+                                        } else if self.playedLetterTwo == "" {
+                                            
+                                            self.playedLetterTwo = self.playerSortedFifthLetter
+                                            
+                                        } else if self.playedLetterThree == "" {
+                                            
+                                            self.playedLetterThree = self.playerSortedFifthLetter
+                                            
+                                        } else if self.playedLetterFour == "" {
+                                            
+                                            self.playedLetterFour = self.playerSortedFifthLetter
+                                            
+                                        } else if self.playedLetterFive == "" {
+                                            
+                                            self.playedLetterFive = self.playerSortedFifthLetter
+                                        }
+                                }//End of TapGesture
+                            
+                            
+                        }//End of Sorted HStack
+                        
+                        
+                    } //End of New Random Word
+                        
+                    if self.playerSortedFirstLetter != "" || self.playerSortedSecondLetter != "" || self.playerSortedThirdLetter != "" || self.playerSortedFourthLetter != "" || self.playerSortedFifthLetter != "" {
+                        
                     
+                        Spacer().frame(height:40)
+                        Text("Played Letters")
+                            .font(.custom("Chalkboard SE", size: 25))
+                        
+                        Group {
+                        HStack {
+                            
+                            PlayedLetterSquare(inletter: self.playedLetterOne)
+                            PlayedLetterSquare(inletter: self.playedLetterTwo)
+                            PlayedLetterSquare(inletter: self.playedLetterThree)
+                            PlayedLetterSquare(inletter: self.playedLetterFour)
+                            PlayedLetterSquare(inletter: self.playedLetterFive)
+                            PlayedLetterSquare(inletter: "🔙")
+                                .onTapGesture {
+                                    if self.playedLetterFive != "" {
+                                        self.playedLetterFive = ""
+                                        
+                                    } else if self.playedLetterFour != "" {
+                                        self.playedLetterFour = ""
+                                        
+                                    } else if self.playedLetterThree != "" {
+                                        self.playedLetterThree = ""
+                                        
+                                    } else if self.playedLetterTwo != "" {
+                                        self.playedLetterTwo = ""
+                                        
+                                    } else if self.playedLetterOne != "" {
+                                        self.playedLetterOne = ""
+                                        
+                                    }
+                            }//End of TapGesture
+                            
+                            
+                            
+                            
+                            
+                        }
+                        }
+                       
+                        
+                    } //End of New Random Word
+                    }
                  
                     
                     
-                }
+                
         }
     }
     
 }
+
+
+//Tile Square model
+struct RandomLetterSquare: View {
+    
+    var inletter = ""
+    
+    var body: some View {
+        
+        ZStack {
+            
+            ZStack {
+            
+            Rectangle()
+                .frame(width:40,height: 40)
+                .foregroundColor(Color.green)
+                .overlay(RoundedRectangle(cornerRadius: 5).stroke(Color.black,lineWidth: 2))
+            
+                Text(inletter)
+                    .foregroundColor(Color.blue)
+                    .font(.system(size: 30))
+            }
+            
+            
+        }
+        
+        
+    }
+    
+}//End of RandomLetter model
+
+
+//Tile Square model
+struct SortedLetterSquare: View {
+    
+    var inletter = ""
+    
+    var body: some View {
+        
+        ZStack {
+            
+            ZStack {
+            
+            Rectangle()
+                .frame(width:40,height: 40)
+                .foregroundColor(Color.orange)
+                .overlay(RoundedRectangle(cornerRadius: 5).stroke(Color.black,lineWidth: 2))
+            
+                Text(inletter)
+                    .foregroundColor(Color.blue)
+                    .font(.system(size: 30))
+            }
+            
+            
+        }
+        
+        
+    }
+    
+}//End of SorfedLetter model
+
+
+
+//Tile Square model
+struct PlayedLetterSquare: View {
+    
+    var inletter = ""
+    
+    var body: some View {
+        
+        ZStack {
+            
+            ZStack {
+            
+            Rectangle()
+                .frame(width:40,height: 40)
+                .foregroundColor(Color.yellow)
+                .overlay(RoundedRectangle(cornerRadius: 5).stroke(Color.black,lineWidth: 2))
+            
+                Text(inletter)
+                    .foregroundColor(Color.blue)
+                    .font(.system(size: 30))
+            }
+            
+            
+        }
+        
+        
+    }
+    
+}//End of PlayedLetter model
